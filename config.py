7208a04a833e22d4d84fcb05eb1f24c70894eb38
RafaeldeLaPuebla <46501857+RafaeldeLaPuebla@users.noreply.github.com>
2019-02-20 20:38:27 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+#Cambiar cuando se suba a producción.
+DEBUG = True
+DATABASE = {
+    'host': 'localhost',
+    'port': 5432,
+    'dbname': 'cryptobook',
+    'dbuser': 'cryptouser',
+    'password': 'Menesiano'
+}
